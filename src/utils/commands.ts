8b41f4f469e220980a7b953f7436a6aa5a63959c
kgrah <kylelgrah@gmail.com>
2024-11-01 20:31:17 -0700

    command updates

--- src/utils/commands.ts
+++ src/utils/commands.ts
@@ -1,69 +1,141 @@
 import packageJson from '../../package.json';
-import themes from '../../themes.json';
 import { history } from '../stores/history';
-import { theme } from '../stores/theme';
 
 const hostname = window.location.hostname;
 
 export const commands: Record<string, (args: string[]) => Promise<string> | string> = {
   help: () => 'Available commands: ' + Object.keys(commands).join(', '),
+  resume : () => {
+    return `
+Kyle Grah
+Senior Back End Software Engineer
+
+-----------------------------------
+Profile
+-----------------------------------
+Senior Software Engineer with 7 years of experience 
+building API-based web services in Golang and 
+AWS/Kubernetes-based applications.
+
+-----------------------------------
+Employment History
+-----------------------------------
+
+Senior Back End Software Engineer
+Inspectiv, Seattle
+December 2022 — July 2024
+
+  - Developed vulnerability detection system in Golang, 
+    driving new customer acquisition revenue and improving 
+    customer satisfaction.
+  - Architected a notifications microservice in Golang, 
+    keeping users updated on software vulnerabilities.
+  - Diagnosed and resolved critical bugs in legacy API, 
+    recovering corrupted production data.
+  - Mentored junior engineers, enhancing professional growth.
+  - Migrated production customer vulnerability database 
+    from GraphQL to PostgreSQL with NATS event streaming 
+    for Inspectiv’s go-to-market rebuild.
+
+Back End Software Engineer
+MASON, Seattle
+December 2020 — September 2022
+
+  - Built WebRTC-based Android remote control streaming 
+    service in Golang, enabling remote debugging for 
+    over 10,000 devices.
+  - Designed a microservice API for storing customer-provided 
+    APKs, media, and config files with reflection-based 
+    validation.
+  - Revamped QA processes, increasing efficiency and 
+    collaboration between backend and QA engineers.
+  - Provided on-call support for a comprehensive system 
+    spanning Golang, JavaScript, Python CLI, and Android.
+
+Full Stack Software Engineer
+Igneous, Seattle
+December 2018 — December 2020
+
+  - Created VM State Machine service using Golang and 
+    Kubernetes, supporting physical customer sites.
+  - Designed System View Filtering in Golang, reducing 
+    UI load times by up to 90%.
+  - Built Data Delete features end-to-end, reducing 
+    ticket volume by 20% for Igneous customers.
+  - Developed API to downsample customer data metrics, 
+    improving load times 10x.
+  - Supported critical customer-facing systems with 
+    24/7 on-call rotations.
+
+Full Stack Software Engineer
+Geniuslink, Seattle
+October 2017 — December 2018
+
+  - Developed BookLinker 2.0, enhancing customer experience 
+    and generating substantial revenue with faster feature 
+    development.
+  - Migrated BookLinker 2.0 to a microservice architecture 
+    with Jenkins, Nomad, and Consul, reducing deployment 
+    time 10x.
+  - Created Product Matching Auditor, achieving 90% accuracy 
+    in product matching with C# API and Vue frontend.
+  - Migrated live customer data from SQL to MongoDB 
+    with Python scripts.
+
+-----------------------------------
+Education
+-----------------------------------
+Bachelor of Information Systems
+University of Washington, Seattle
+September 2013 — June 2017
+
+-----------------------------------
+Details
+-----------------------------------
+Location: Seattle
+Email:    kylelgrah@gmail.com
+
+-----------------------------------
+Skills
+-----------------------------------
+Golang | ██████████
+Web APIs | ████████
+Distributed Systems Architecture | ████████
+SQL | ████████
+TypeScript | ███████
+Event Streaming | ███████
+React | ██████
+AWS | ██████
+Jenkins | █████
+Kubernetes | █████
+WebRTC | █████
+Python | █████
+Redis | █████
+JavaScript | █████
+Docker | █████
+MongoDB | ████
+Linux | ████
+Security Vulnerabilities | ████
+Protobuf | ████
+NATS | ████
+
+    `
+  },
   hostname: () => hostname,
   whoami: () => 'guest',
   date: () => new Date().toLocaleString(),
-  vi: () => `why use vi? try 'emacs'`,
-  vim: () => `why use vim? try 'emacs'`,
-  emacs: () => `why use emacs? try 'vim'`,
   echo: (args: string[]) => args.join(' '),
+  learn: () => {
+    window.open('https://www.geeksforgeeks.org/linux-commands-cheat-sheet/')
+    
+    return 'Opening Linux commands cheat sheet...';
+  },
   sudo: (args: string[]) => {
-    window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+    window.open('');
 
     return `Permission denied: unable to run the command '${args[0]}' as root.`;
   },
-  theme: (args: string[]) => {
-    const usage = `Usage: theme [args].
-    [args]:
-      ls: list all available themes
-      set: set theme to [theme]
-
-    [Examples]:
-      theme ls
-      theme set gruvboxdark
-    `;
-    if (args.length === 0) {
-      return usage;
-    }
-
-    switch (args[0]) {
-      case 'ls': {
-        let result = themes.map((t) => t.name.toLowerCase()).join(', ');
-        result += `You can preview all these themes here: ${packageJson.repository.url}/tree/master/docs/themes`;
-
-        return result;
-      }
-
-      case 'set': {
-        if (args.length !== 2) {
-          return usage;
-        }
-
-        const selectedTheme = args[1];
-        const t = themes.find((t) => t.name.toLowerCase() === selectedTheme);
-
-        if (!t) {
-          return `Theme '${selectedTheme}' not found. Try 'theme ls' to see all available themes.`;
-        }
-
-        theme.set(t);
-
-        return `Theme set to ${selectedTheme}`;
-      }
-
-      default: {
-        return usage;
-      }
-    }
-  },
-  repo: () => {
+  github: () => {
     window.open(packageJson.repository.url, '_blank');
 
     return 'Opening repository...';
@@ -74,14 +146,7 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
     return '';
   },
   email: () => {
-    window.open(`mailto:${packageJson.author.email}`);
-
-    return `Opening mailto:${packageJson.author.email}...`;
-  },
-  donate: () => {
-    window.open(packageJson.funding.url, '_blank');
-
-    return 'Opening donation url...';
+    return `kylelgrah@gmail.com`;
   },
   weather: async (args: string[]) => {
     const city = args.join('+');
@@ -95,7 +160,7 @@ export const commands: Record<string, (args: string[]) => Promise<string> | stri
     return weather.text();
   },
   exit: () => {
-    return 'Please close the tab to exit.';
+    return 'window.close()';
   },
   curl: async (args: string[]) => {
     if (args.length === 0) {
